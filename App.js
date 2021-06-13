2f3f4c78617d1e52f802a0b0cf868f15fcaf111c
tharunvignesh <67272288+tharunvignesh@users.noreply.github.com>
2021-06-13 17:05:13 +0530

    add files

---
+++ App.js
@@ -0,0 +1,7 @@
+// Navbar Toggle
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
+
+navBarToggle.addEventListener("click", function () {
+  mainNav.classList.toggle("active");
+});
